77f8d557c8508e88fd489604a90c6b2f067b9015
rubentxogg <rubentxogg@hotmail.com>
2022-06-07 19:09:25 +0200

    [ADD] Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '8deaa31b8dmsh5b87e23e302820fp172d2ajsn4050dd4c5bf7'
 };
